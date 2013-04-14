6c713a777eb2fe612dbd52774562dd2f4959bc46
Anton Dachauer <kontakt@lacemedia.de>
2013-04-14 02:40:44 +0200

    admin

---
+++ admin/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Admin - pleep.net</title>
+<link rel="stylesheet" href="/css/style.css.php" />
+</head>
+
+<body>
+
+<div class="container">
+	<form class="form-signin" action="/login.php" method="post">
+		<h2 class="form-signin-heading">Administration</h2>
+		<input type="text" class="input-block-level" placeholder="Email address">
+		<input type="password" class="input-block-level" placeholder="Password">
+		<button class="btn btn-large btn-primary" type="submit">Anmelden</button>
+	</form>
+</div>
